5c6f2a238c87dc0c2e8de96b8a908306c092211f
M.Kichman <w57015@student.wsiz.rzeszow.pl>
2019-02-04 18:43:12 +0100

    recipe details and shopping list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -8,9 +8,13 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   recipes: Recipe[] = [
     new Recipe('Test', 'This is a test',
+     'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg' ),
+
+    new Recipe('Test', 'This is a another test',
      'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg' )
   ];
 
@@ -19,4 +23,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
